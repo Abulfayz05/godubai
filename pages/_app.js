2e4c987e324c40ee0c00572833b3d526b63a626e
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-11-10 19:59:38 +0500

    change ico

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
